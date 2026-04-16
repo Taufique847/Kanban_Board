5ab64ed43ec2cdd84ef9fedf651c9b8346c5a71e
Md Taufique <mdtaufique5300@gmail.com>
2026-04-16 19:16:35 +0530

    status

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: {
     // appIsrStatus: false,
-    // buildActivity: false,
+    buildActivity: false,
   },
 };
 
